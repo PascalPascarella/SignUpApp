5a9fe45ff2dfc44bf640bc095a049e6350241f6f
Pascal Pascarella <pascalpascarella86@gmail.com>
2021-01-16 03:36:28 -0600

    Create SignUpScreen class containing SignUpForm method with initial styles

--- sign_in_app/lib/main.dart
+++ sign_in_app/lib/main.dart
@@ -12,3 +12,19 @@ class SignUpApp extends StatelessWidget {
     );
   }
 }
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      )
+    )
+  }
+}
