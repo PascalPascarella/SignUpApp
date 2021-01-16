a862197ca12616f57a22753f80585ab29bac4b49
Pascal Pascarella <pascalpascarella86@gmail.com>
2021-01-16 04:08:57 -0600

    Build Form first name block

--- sign_in_app/lib/main.dart
+++ sign_in_app/lib/main.dart
@@ -39,4 +39,32 @@ class _SignUpFormState extends State<SignUpForm> {
   final _firstNameTextController = TextEditingController();
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
+
+  double _formProgress = 0;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LinearProgressIndicator(
+            value: _formProgress),
+          Text("Sign Up", style: Theme
+            .of(context)
+            .textTheme
+            .headline4),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _firstNameTextController,
+              decoration: InputDecoration(hintText: "First Name"),
+            ),
+          )
+          
+          ]
+          ,
+          )
+          )
+  }
 }
